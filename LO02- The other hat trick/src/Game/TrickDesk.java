c0f8c2831832ccea63e73ab36883f4d1c3939f75
rinergan <phuc_loc.luong@utt.fr>
2018-11-27 23:54:10 +0100

    Commentaire

--- LO02- The other hat trick/src/Game/TrickDesk.java
+++ LO02- The other hat trick/src/Game/TrickDesk.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 
 import Card.Trick;
 
-public class TrickDesk extends ArrayList<Trick> {
+public class TrickDesk extends ArrayList<Trick> { //TrickDesk est une liste de Trick
 	
 	private Match match;
-
+	//Methode pour créer le TrickDesk au début d'un Match 
 	public TrickDesk (Match matchActuel, ArrayList<Trick> tricks) {		
 		super();
 		this.match = matchActuel;
 		this.addAll(tricks);
 		this.flipTrick();
 	}
+	//Methode pour retourner la carte en top de TrickDesk et puis l'envoyer vers Trick Pile
 	public void flipTrick() {
 		Trick trickTop = this.get(0);
 		trickTop.setIsFaceUp(true);
