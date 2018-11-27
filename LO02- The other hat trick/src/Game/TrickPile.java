c3fa5761da87672e5099199f8926254c377dc972
rinergan <phuc_loc.luong@utt.fr>
2018-11-27 23:56:27 +0100

    Commenter

--- LO02- The other hat trick/src/Game/TrickPile.java
+++ LO02- The other hat trick/src/Game/TrickPile.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 import Card.Trick;
 
-public class TrickPile extends ArrayList<Trick>{
+public class TrickPile extends ArrayList<Trick>{ //TrickPile est une liste de Trick 
 	
 	private Match match;
-
+	//Methode pour créer l'objet TrickPile
 	public TrickPile (Match matchActuel) {
 		
 		super();
